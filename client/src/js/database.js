45c9217308bd1d54e7f8f8bcf83c8177b14f1cdb
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-08-30 00:16:31 -0400

    so stuff is now being saved in jate index but it isnt returning on reload also none of my imaghes are appearing due too how webpack hashes all assets so dumb another thing is that it is having an issue with inject manoiifest being called to many times

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,43 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await initdb(); // Initialize the database
+  const tx = db.transaction('jate', 'readwrite'); // Start a transaction
+  const store = tx.objectStore('jate'); // Get the object store
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Add the content to the store
+  const newItem = { content };
+  const key = await store.add(newItem);
+
+  await tx.complete; // Complete the transaction
+  console.log('Added content to the database with key:', key);
+};
+
+export const getDb = async () => {
+  const db = await initdb(); // Initialize the database
+  const tx = db.transaction('jate', 'readonly'); // Start a read-only transaction
+  const store = tx.objectStore('jate'); // Get the object store
+
+  const contentArray = [];
+
+  return new Promise((resolve, reject) => {
+    const cursorRequest = store.openCursor();
+    cursorRequest.onsuccess = (event) => {
+      const cursor = event.target.result;
+      if (cursor) {
+        contentArray.push(cursor.value.content);
+        cursor.continue(); // Move to the next item
+      } else {
+        resolve(contentArray); // Resolve the promise when all items are processed
+      }
+    };
+
+    cursorRequest.onerror = (event) => {
+      reject(event.target.error); // Reject the promise if an error occurs
+    };
+  });
+};
 
 initdb();
