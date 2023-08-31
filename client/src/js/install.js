d405be6f05e0260cc92f03338310e6159c6733a9
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-08-30 21:34:06 -0400

    app now installs without the install button now the only thing i need to do is figure out why manifest is compiling images that the index isnt grabbing so frustratinf it should be working

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,25 @@
 const butInstall = document.getElementById('buttonInstall');
-
+let installPrompt;
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+  installPrompt = event;
+  butInstall.removeAttribute("hidden");
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (!installPrompt) {
+        return;
+      }
+      const result = await installPrompt.prompt();
+      console.log(`Install prompt was: ${result.outcome}`);
+      disableInAppInstallPrompt();
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log("Thank you for installing our app!");
+    butInstall.remove();
+});
